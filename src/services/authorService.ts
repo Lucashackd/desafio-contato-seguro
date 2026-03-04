82b2103937d6b7b4b9cf99e982326d10862a84d9
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2026-03-04 17:53:15 -0300

    feat: implement author and book management functions in services

--- src/services/authorService.ts
+++ src/services/authorService.ts
@@ -1,6 +1,25 @@
 import localforage from "localforage";
+import type { Author, CreateAuthorDto } from "../types/author";
 
 const db = localforage.createInstance({
   name: "biblioteca",
   storeName: "authors",
 });
+
+export const addAuthor = async (data: CreateAuthorDto): Promise<Author> => {
+  const authors = await getAuthors();
+  const newAuthor = { id: crypto.randomUUID(), ...data };
+  await db.setItem("list", [...authors, newAuthor]);
+  return newAuthor;
+};
+
+export const deleteAuthor = async (id: string): Promise<void> => {
+  const authors = await getAuthors();
+  const updatedAuthors = authors.filter((author) => author.id !== id);
+  await db.setItem("list", updatedAuthors);
+};
+
+export const getAuthors = async (): Promise<Author[]> => {
+  const authors: Author[] = (await db.getItem<Author[]>("list")) ?? [];
+  return authors;
+};
--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -1,6 +1,25 @@
 import localforage from "localforage";
+import type { Book, CreateBookDto } from "../types/book";
 
 const db = localforage.createInstance({
   name: "biblioteca",
   storeName: "books",
 });
+
+export const addBook = async (data: CreateBookDto): Promise<Book> => {
+  const books = await getBooks();
+  const newBook = { id: crypto.randomUUID(), ...data };
+  await db.setItem("list", [...books, newBook]);
+  return newBook;
+};
+
+export const deleteBook = async (id: string): Promise<void> => {
+  const books = await getBooks();
+  const updatedBooks = books.filter((book) => book.id !== id);
+  await db.setItem("list", updatedBooks);
+};
+
+export const getBooks = async (): Promise<Book[]> => {
+  const books: Book[] = (await db.getItem<Book[]>("list")) ?? [];
+  return books;
+};
